a51cc2eaab5cc8101d5d62bf4cf57943177001b3
rahman2600 <abdurrahmanmudasiru@yahoo.com>
2021-07-29 15:15:29 +0100

    try

--- app.js
+++ app.js
@@ -24,4 +24,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
 module.exports = app;
